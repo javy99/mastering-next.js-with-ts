37d26e86380583390c0a8e614ef7a8a0b6423a2c
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-08-30 22:01:32 +0200

    Caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,7 +4,10 @@ interface User {
 }
 
 const UsersPage = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const res = await fetch("https://jsonplaceholder.typicode.com/users",
+        // { cache: 'no-store' } // disable caching
+        { next: { revalidate: 10 } } // this means that next.js will run background job and fetch data from the backend every 10 seconds. So, this is how caching works in Next.js. Now, one thing we need to know is that this behavior is only implemented in the fetch function . So, if we use a third-party library, like axios, we are not gonna get this, we are not gonna get the data cache.
+    )
     const users: User[] = await res.json()
 
     return (
