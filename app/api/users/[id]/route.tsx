b05b359254f7021127702b65f3b0215a1486c152
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-08-31 11:51:34 +0200

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -2,9 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function GET(
     request: NextRequest,
-    { params: { id } }: {
-        params: { id: number }
-    }) {
+    { params: { id } }: { params: { id: number } }) {
     // Fetch data from a db
     // If not found, return 404 error
     // Else return data
@@ -12,4 +10,26 @@ export function GET(
         return NextResponse.json({ error: 'User not found' }, { status: 404 })
     else
         return NextResponse.json({ id: 1, name: 'Javy' })
+}
+
+// PUT   - for replacing object
+// PATCH - for updating 1 or more properties
+
+export async function PUT(
+    request: NextRequest,
+    { params: { id } }: { params: { id: number } }) {
+    // Validate the request body
+    const body = await request.json()
+    // If invalid, return 400
+    if (!body.name)
+        return NextResponse.json({ error: 'Name is required' }, { status: 400 })
+    // Fetch the user with the given id
+
+    // If doesn't exist, return 404
+    if (id > 10)
+        return NextResponse.json({ error: 'User not found' }, { status: 404 })
+    // Update the user
+    // Return the updated user 
+    return NextResponse.json({ id: 1, name: body.name })
+
 }
