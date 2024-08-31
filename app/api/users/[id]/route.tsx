2306f4f1cfc6a057962b978be1f58ed0f503b364
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-08-31 14:06:17 +0200

    Deleting Data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -43,8 +43,17 @@ export async function PUT(
 
 export async function DELETE(
     request: NextRequest,
-    { params: { id } }: { params: { id: number } }) {
-    if (id > 10)
+    { params: { id } }: { params: { id: string } }) {
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(id) }
+    })
+
+    if (!user)
         return NextResponse.json({ error: 'User not found' }, { status: 404 })
+
+    await prisma.user.delete({
+        where: { id: user.id }
+    })
+
     return NextResponse.json({});
 }
