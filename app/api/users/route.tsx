6c2025b96c5c7d72c7fb14e1d932732b676c578a
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-08-31 13:27:52 +0200

    Creating Data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -3,11 +3,6 @@ import schema from "./schema";
 import prisma from "@/prisma/client";
 
 export async function GET(request: NextRequest) {
-    // prisma.user.findMany({
-    //     where: {
-    //         email: ''
-    //     }
-    // })
     const users = await prisma.user.findMany()
 
     return NextResponse.json(users);
@@ -19,5 +14,17 @@ export async function POST(request: NextRequest) {
     if (!validation.success)
         return NextResponse.json(validation.error.errors, { status: 400 })
 
-    return NextResponse.json({ id: 1, name: body.name }, { status: 201 })
+    const user = await prisma.user.findUnique({
+        where: { email: body.email }
+    })
+    if (user)
+        return NextResponse.json({ error: 'User already exists.' }, { status: 400 })
+
+    const newUser = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    })
+    return NextResponse.json(newUser, { status: 201 })
 }
