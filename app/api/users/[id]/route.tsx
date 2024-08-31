8939d77ab053b63a0cf057ccd7169aa95a7cae4f
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-08-31 14:00:22 +0200

    Updating Data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -17,15 +17,28 @@ export async function GET(
 
 export async function PUT(
     request: NextRequest,
-    { params: { id } }: { params: { id: number } }) {
+    { params: { id } }: { params: { id: string } }) {
     const body = await request.json()
     const validation = schema.safeParse(body);
     if (!validation.success)
         return NextResponse.json(validation.error.errors, { status: 400 })
 
-    if (id > 10)
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(id) }
+    })
+
+    if (!user)
         return NextResponse.json({ error: 'User not found' }, { status: 404 })
-    return NextResponse.json({ id: 1, name: body.name })
+
+    const updatedUser = await prisma.user.update({
+        where: { id: user.id },
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    })
+
+    return NextResponse.json(updatedUser)
 }
 
 export async function DELETE(
