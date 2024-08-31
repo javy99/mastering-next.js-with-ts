785fc1ff56e2603d600aa88c576020f0fdd32f75
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-08-31 11:45:00 +0200

    Creating an Object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -9,5 +9,15 @@ export function GET(request: NextRequest) {
         { id: 1, name: 'Mosh' },
         { id: 2, name: 'John' }
     ]);
+}
 
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    // Validate
+    // If invalid, return 400
+    // Else, return data
+    if (!body.name)
+        return NextResponse.json({ error: 'Name is required' }, { status: 400 })
+
+    return NextResponse.json({ id: 1, name: body.name }, { status: 201 })
 }
