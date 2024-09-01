0c33dcd030cd90b31c572716446e33518bff1145
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-01 13:34:17 +0200

    Styling Emails - 1

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,17 +1,26 @@
-import { Html, Body, Container, Text, Link, Preview } from "@react-email/components"
+import { Html, Body, Container, Tailwind, Text, Link, Preview } from "@react-email/components"
+import { CSSProperties } from "react"
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
     return (
         <Html>
             <Preview>Welcome aboard!</Preview>
-            <Body>
+            <Body style={body}>
                 <Container>
-                    <Text>Hello {name}</Text>
+                    <Text style={heading}>Hello {name}</Text>
                     <Link href='https://codewithmosh.com'>www.codewithjavy.com</Link>
                 </Container>
             </Body>
         </Html>
     )
+}
+
+const body: CSSProperties = {
+    background: '#fff'
+}
+
+const heading: CSSProperties = {
+    fontSize: '24px',
 }
 
 export default WelcomeTemplate
