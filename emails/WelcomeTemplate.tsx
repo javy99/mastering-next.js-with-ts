f0e75c2d963655747303e44a8bfe69cf885054c0
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-01 13:26:00 +0200

    Creating an Email Template

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,17 @@
+import { Html, Body, Container, Text, Link, Preview } from "@react-email/components"
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+    return (
+        <Html>
+            <Preview>Welcome aboard!</Preview>
+            <Body>
+                <Container>
+                    <Text>Hello {name}</Text>
+                    <Link href='https://codewithmosh.com'>www.codewithjavy.com</Link>
+                </Container>
+            </Body>
+        </Html>
+    )
+}
+
+export default WelcomeTemplate
